952dc921214c8098ab99d1f649324b0735c08715
Kaleth Cristancho <48393210+KaJoseh@users.noreply.github.com>
2021-01-30 15:53:53 -0500

    2D sprites y Transiciones
    Se agregaron los paquetes 2d de unity y actualizaron algunos otros como el TextMeshPro.
    
    Transición de escena Menú a Introducción hecha.

---
+++ Assets/Scripts/Behaviours/SceneChangerKey.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class SceneChangerKey : MonoBehaviour {
+
+	[Header("Clip de audio:")]
+	public AudioClip backs;
+
+	[Header("El nivel donde debemos ir:")]
+	public string nivel;
+
+	[Header("La tecla que presionamos para ir:")]
+	public KeyCode tecla;
+
+	private AudioSource audioSource;
+
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	private IEnumerator OpenScene ( string escena ) {
+		float fadeTime = GameObject.Find ("Canvas").GetComponent<Fading> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene (escena);
+	}
+	void Update () {
+		if(Input.GetKeyDown(tecla)){
+			audioSource.clip = backs;
+			audioSource.Play();
+			StartCoroutine(OpenScene (nivel));
+		}
+	}
+}
